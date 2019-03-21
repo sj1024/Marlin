350d6af9b8f195c50bb6073542ff01d4d294bad9
john.yoon@emthink.com <john.yoon@emthink.com>
2019-03-22 08:49:36 +0900

    Y Probe offset fixed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -775,7 +775,7 @@
  *    (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -44  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -4  // Y offset: -front +behind [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -10  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
